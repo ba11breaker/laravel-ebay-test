9998ec2d0f1d41db8b877331379d53ef6083267c
vagrant <vagrant@homestead>
2020-04-20 04:55:39 +0000

    init layout

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -2,8 +2,22 @@
 <html>
   <head>
     <title>@yield('title', 'Ebay App')</title>
+    <link rel="stylesheet" href="/css/app.css">
   </head>
   <body>
-    @yield('content')
+
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div class="container">
+        <a class="navbar-brand" href="/">Ebay App</a>
+        <ul class="navbar-nav justify-content-end">
+          <li class="nav-item"><a class="nav-link" href="#">Help</a></li>
+          <li class="nav-item" ><a class="nav-link" href="#">Log In</a></li>
+        </ul>
+      </div>
+    </nav>
+
+    <div class="container">
+      @yield('content')
+    </div>
   </body>
 </html>
