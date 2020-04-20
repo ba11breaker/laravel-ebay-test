05358548ad46a10b4e493227fce684e674357451
vagrant <vagrant@homestead>
2020-04-20 05:03:55 +0000

    cut header sub-page

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -2,22 +2,16 @@
 <html>
   <head>
     <title>@yield('title', 'Ebay App')</title>
-  <link rel="stylesheet" href="{{ mix('css/app.css' )}}">
+    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
   </head>
-  <body>
 
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-      <div class="container">
-        <a class="navbar-brand" href="/">Ebay App</a>
-        <ul class="navbar-nav justify-content-end">
-          <li class="nav-item"><a class="nav-link" href="#">Help</a></li>
-          <li class="nav-item" ><a class="nav-link" href="#">Log In</a></li>
-        </ul>
-      </div>
-    </nav>
+  <body>
+    @include('layouts._header')
 
     <div class="container">
-      @yield('content')
+      <div class="offset-md-1 col-md-10">
+        @yield('content')
+      </div>
     </div>
   </body>
 </html>
