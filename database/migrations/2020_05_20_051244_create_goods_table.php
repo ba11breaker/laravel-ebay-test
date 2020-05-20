7130f61d97ee93258364975cfefbee363fd07c9d
vagrant <vagrant@homestead>
2020-05-20 05:34:59 +0000

    good model

---
+++ database/migrations/2020_05_20_051244_create_goods_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateGoodsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('goods', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->text('name');
+            $table->text('description');
+            $table->text('picurl');
+            $table->integer('user_id')->index();
+            $table->integer('favourites')->default(0);
+            $table->index(['created_at']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('goods');
+    }
+}
